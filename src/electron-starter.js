bef7ddbfa5b540f154dde2abd2dc9ceb718f4e4e
yuemori <yuemori@aiming-inc.com>
2018-05-28 13:29:14 +0900

    Apply develop settings

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -10,10 +10,10 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('http://localhost:3000')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
