537a92e8a0bd41f113ab81f20c15c8e6dbe3237b
yuemori <yuemori@aiming-inc.com>
2018-06-03 22:40:35 +0900

    Show repository history

--- src/RepositorySelector.js
+++ src/RepositorySelector.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
-import { Button, Alert } from 'reactstrap';
+import { Button, Alert, ListGroup, ListGroupItem, Badge } from 'reactstrap';
+const NodeGit = require('nodegit');
+
 const { dialog } = window.require('electron').remote;
 
 export default class RepositorySelector extends Component {
   constructor() {
     super();
-    this.state = { repositoryPath: 'NotSelected', isSelected: false };
+    this.state = { repositoryPath: 'NotSelected', isSelected: false, commits: [] };
   }
 
-  openDialog = () => {
-    dialog.showOpenDialog({ properties: ['openDirectory'], title: 'Gitリポジトリを選択', }, (folderName) => {
-      this.setState({ repositoryPath: folderName, isSelected: true });
+  onOpenDialog() {
+    const folderName = dialog.showOpenDialog({ properties: ['openDirectory'], title: 'Gitリポジトリを選択', })[0];
+    this.setState({ repositoryPath: folderName, isSelected: true });
+    NodeGit.Repository.open(folderName + '/.git').then((repository) => {
+      console.log(repository);
+      return repository;
+    })
+    .then((repository) => {
+      return repository.getHeadCommit();
+    })
+    .then((firstCommit) => {
+      const history = firstCommit.history();
+      history.on('end', async (commits) => {
+        this.setState({ commits: commits });
+      });
+      history.start();
+    })
+    .catch((err) => {
+      console.log(err);
     });
   }
 
@@ -18,9 +36,14 @@ export default class RepositorySelector extends Component {
     return (
       <div>
         <Alert color={this.state.isSelected ? 'primary' : 'warning'}>
-          <Button color="info" size="sm" onClick={this.openDialog} className="mr-2">Open</Button>
+          <Button color="info" size="sm" onClick={(e) => this.onOpenDialog(e)} className="mr-2">Open</Button>
           <span>RepositoryPath: {this.state.repositoryPath}</span>
         </Alert>
+        <ListGroup>
+        {this.state.commits.map((commit) => {
+          return <ListGroupItem key={commit.sha()}><Badge pill>{commit.sha().slice(0, 6)}</Badge> {commit.message()}</ListGroupItem>
+        })}
+        </ListGroup>
       </div>
     )
   }
