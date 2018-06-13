fb64571b6d6d80d15697e88e503feda279531773
yuemori <yuemori@aiming-inc.com>
2018-06-13 22:43:03 +0900

    Refactor(wip)

--- src/components/History.js
+++ src/components/History.js
@@ -2,160 +2,74 @@ import React, { Component } from 'react';
 import { Table } from 'reactstrap';
 import InfiniteScroll from 'react-infinite-scroller';
 
-class TreeRow {
-  constructor() {
-    this.columns = [];
-  }
+const dotStrokeWidth = 5;
+const contentHeight = 30;
 
-  map(callback) {
-    return this.columns.map(callback)
-  }
+const nextColor = () => {
+  // TODO: implement
+  return "#000000";
+}
 
-  register(columns) {
-    columns.forEach((column) => {
-      this.columns.push({
-        commit: column.commit,
-        oid: column.oid,
-        x: column.x
-      });
-    });
+const commitBuilder = (commit) => {
+  return {
+    message: commit.message(),
+    sha: commit.id().tostrS(),
+    author: commit.author().name(),
+    date: commit.date(),
+    parentOids: commit.parents().map(oid => oid.tostrS()),
   }
 }
 
-class TreeColumn {
-  constructor(x) {
-    this.x = x;
-  }
-
-  reserve(oid) {
-    this.oid = oid.tostrS();
-  }
-
-  register(commit) {
-    this.commit = commit;
-    this.reserve(commit.id())
-  }
+const GraphBuilder = (commits) => {
+  // TODO
+  return [];
 }
 
-const treeBuilder = (commits) => {
-  const columns = [];
+const Node = (props) => {
+  const {x, y} = props;
+  const cx = 15 + x * 15;
+  const cy = 15 + contentHeight * y;
 
-  for(let i=0;i<30;i++) {
-    columns.push(new TreeColumn(i));
-  }
-  const rows = [];
-
-  const findColumnByOid = (oid) => {
-    return columns.find(c => c.oid === oid.tostrS());
-  }
-
-  const findEmptyColumn = () => {
-    return columns.find(c => c.oid == null);
-  }
-
-  commits.forEach(commit => {
-    const row = new TreeRow();
-    let column = findColumnByOid(commit.id())
-
-    if(column == null) {
-      column = findEmptyColumn();
-    }
-
-    column.register(commit);
-    row.register(columns)
-    rows.push(row);
-    columns.forEach(c => c.commit = null);
-    column.oid = null
-
-    const parents = commit.parents();
-
-    if(parents.length !== 0) {
-      if(findColumnByOid(parents[0]) == null) {
-        column.reserve(parents[0]);
-      }
-    }
-
-    parents.slice(1, parents.length).forEach((p) => {
-      let column = findColumnByOid(p)
-
-      if(column == null) {
-        findEmptyColumn().reserve(p);
-      }
-    });
-  })
-
-  return rows;
+  return <circle cx={cx} cy={cy} r={dotStrokeWidth} />;
 }
 
-class Node extends Component {
-  render() {
-    const {x, y} = this.props;
-    const dotStrokeWidth = 5;
-    const contentHeight = 30;
-    const cx = 15 + x * 15;
-    const cy = 15 + contentHeight * y;
+const Row = (props) => {
+  return (
+    <g>
+      {props.columns.map((column, x) => {
+        // console.log(`commit: ${column.commit}, x: ${x}`)
 
-    return (
-      <circle cx={cx} cy={cy} r={dotStrokeWidth}/>
-    )
-  }
+        if(column.commit != null) {
+          return <Node key={x} x={x} y={props.y} />
+        }
+
+        return null;
+      })}
+    </g>
+  )
 }
+const Graph = (props) => {
+  const maxY = props.tree.length * contentHeight;
 
-class Row extends Component {
-  render() {
-    const {row, y} = this.props;
-
-    return (
-      <g>
-        {row.map(column => {
-          return (column.commit != null) ? <Node key={column.x} x={column.x} y={y} /> : null
-        })}
-      </g>
-    )
-  }
-}
-
-const calcGraphWidth = (tree) => {
-  const min = 90;
-  const maxX = Math.max(...tree.map(row => Math.max(...row.columns.filter(column => column.commit != null).map(column => column.x))));
-
-  if(maxX === -Infinity) {
-    return min;
-  }
-
-  return (maxX * 30 > min) ? maxX * 30 : min;
-}
-
-class Graph extends Component {
-  render() {
-    const {tree} = this.props;
-
-    console.log(tree);
-
-    const width = calcGraphWidth(tree);
-
-    return (
-      <svg height={tree.length * 30} width={width}>
-        {tree.map((row, y) => {
-          return <Row key={y} row={row} y={y} />
-        })}
-      </svg>
-    )
-  }
+  return (
+    <svg height={maxY}>
+    {props.tree.map((columns, y) => {
+      return <Row key={y} y={y} columns={columns} />
+    })}
+    </svg>
+  )
 }
 
 export default class History extends Component {
   render() {
     const {loadFunc, hasMore, commits} = this.props;
-    console.log(commits);
-    const tree = treeBuilder(commits);
+    const tree = GraphBuilder(commits);
     const style = {
       overflow: "scroll",
       height: "300px"
     };
 
-    const width = calcGraphWidth(tree);
-    console.log(width);
+    const width = 300;
 
     return (
       <div style={style}>
